f789e30dac5446c0c44a0b2d6a823a9c53cb7440
nydragon <git@ccnlc.eu>
2026-06-26 22:55:38 +0200

    Update to uv2nix

--- flake.nix
+++ flake.nix
@@ -1,46 +1,106 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     {
-      self,
       nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        python = pkgs.python314;
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.uv
-            python
-            (python.pkgs.nox.overridePythonAttrs (old: {
-              # Propagating dependencies leaks them through $PYTHONPATH which causes issues
-              # when used in nix-shell.
-              postFixup = ''
-                rm $out/nix-support/propagated-build-inputs
-              '';
-            }))
-          ];
+      pyproject-nix,
+      uv2nix,
+      pyproject-build-systems,
+      flake-parts,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        {
+          lib,
+          pkgs,
+          ...
+        }:
+        let
 
-          SOURCE_DATE_EPOCH = "315532800"; # The year 1980
-          PYTHONBREAKPOINT = "IPython.terminal.debugger.set_trace";
-          UV_NO_MANAGED_PYTHON = "1";
+          workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
 
-          shellHook = ''
-            set -ex
+          overlay = workspace.mkPyprojectOverlay {
+            sourcePreference = "wheel";
+          };
 
-            VENV_BIN_DIR=$(nox -vv -s dev_env --force-python ${python.pythonVersion})
-            source "$VENV_BIN_DIR/activate"
-          '';
+          editableOverlay = workspace.mkEditablePyprojectOverlay {
+            root = "$REPO_ROOT";
+          };
+
+          pyprojectOverrides = _final: _prev: {
+            # - https://pyproject-nix.github.io/uv2nix/FAQ.html
+            # Implement build fixups here.
+          };
+
+          pythonSet =
+            let
+              python = pkgs.python3;
+            in
+            (pkgs.callPackage pyproject-nix.build.packages {
+              inherit python;
+            }).overrideScope
+              (
+                lib.composeManyExtensions [
+                  pyproject-build-systems.overlays.wheel
+                  overlay
+                  pyprojectOverrides
+                ]
+              );
+
+        in
+        {
+          packages.default = pythonSet.mkVirtualEnv "instawow" workspace.deps.default;
+
+          devShells =
+            let
+              pythonSet' = pythonSet.overrideScope editableOverlay;
+              python = pythonSet.python.interpreter;
+              virtualenv = pythonSet'.mkVirtualEnv "instawow-dev-env" workspace.deps.all;
+            in
+            {
+              default = pkgs.mkShell {
+                packages = [
+                  virtualenv
+                  pkgs.uv
+                ];
+
+                env = {
+                  # UV_NO_SYNC = "1";
+                  UV_PYTHON = python;
+                  UV_PYTHON_DOWNLOADS = "never";
+                  LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+                };
+
+                shellHook = ''
+                  unset PYTHONPATH
+                  export REPO_ROOT=$(git rev-parse --show-toplevel)
+                '';
+              };
+            };
         };
-      }
-    );
+    };
 }
